2211769acb690deb0e342090bd788c6aef71b7b6
Ubuntu <jeff@ubuntu.(none)>
2014-03-01 12:35:26 -0800

    startup script now cleans up child processes

--- scripts/startup.py
+++ scripts/startup.py
@@ -2,9 +2,9 @@
 
 import argparse
 import json
-#from ssh import SSHClient
 import subprocess 
 import sys
+import os
 
 parser = argparse.ArgumentParser(description='startup script for DecaFS')
 parser.add_argument('-f',
@@ -36,13 +36,30 @@ for node in data["nodes"]["espresso"]:
 
 # deal with handling incorrecly formatted config files here
 
-#subprocess.check_output(["ssh", "pi@" + barista_ip, "touch testfile"])
-#barista = subprocess.Popen(["ssh", "pi@" + barista_ip, "echo Barista is: `cat /etc/hostname`"], stdout=sys.stdout)
-barista = subprocess.Popen(["ssh", "pi@" + barista_ip, "./decafs_barista"], stdout=sys.stdout)
-barista.communicate()
+# fork barista here
+pids=[]
 
-espresso=[]
+pid = os.fork()
+if pid == 0: # if in child
+   #barista = subprocess.Popen(["ssh", "pi@" + barista_ip, "echo Barista is: `cat /etc/hostname`"], stdout=sys.stdout)
+   barista = subprocess.Popen(["ssh", "pi@" + barista_ip, "./decafs_barista"], stdout=sys.stdout)
+   barista.communicate()
+   exit(0)
+else:
+   pids.append(pid)
+
 for (i, ip) in enumerate(espresso_ips):
    #espresso.append(subprocess.Popen(["ssh", "pi@" + ip, "echo Espresso " + str(i+1) + ": `cat /etc/hostname`"], stdout=sys.stdout))
-   espresso.append(subprocess.Popen(["ssh", "pi@" + ip, "./decafs_espresso"], stdout=sys.stdout))
-   espresso[len(espresso)-1].communicate()
+   pid = os.fork()
+
+   if pid == 0:
+      espresso=subprocess.Popen(["ssh", "pi@" + ip, "./decafs_espresso " + ip], stdout=sys.stdout)
+      espresso.communicate()
+      exit(0)
+   else:
+      pids.append(pid)
+
+count = 0
+while (count < len(pids)):
+   os.waitpid(0, 0)
+   count = count + 1
