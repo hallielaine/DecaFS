39f7c065ffcc8a5f83499695ba0aa2c738af678c
Halli <hallielaine@gmail.com>
2014-03-04 11:04:36 -0800

    Work on Vmeta (may break build - desktop problems)

--- src/app/barista_core/volatile_metadata.cpp
+++ src/app/barista_core/volatile_metadata.cpp
@@ -3,8 +3,6 @@
 Volatile_Metadata::Volatile_Metadata() {
   // initialize volatile metadata map for file cursor
 
-  // intialize node map
-  
   // Initialize stripe and chunk size
   chunk_size = 0;
   stripe_size = 0;
@@ -40,4 +38,49 @@ int Volatile_Metadata::set_stripe_size (uint32_t size) {
 
 uint32_t Volatile_Metadata::get_stripe_size() {
   return stripe_size;
+}
+
+uint32_t Volatile_Metadata::get_node_number (char *ip) {
+  if (ip_to_node_map_contains (ip)) {
+    return ip_to_node_map[ip];
+  }
+  return IP_NOT_FOUND;
+}
+
+int Volatile_Metadata::add_node (char *ip, uint32_t node_number) {
+  if (!ip_to_node_map_contains (ip)) {
+    ip_to_node_map[ip] = node_number;
+    up_nodes.insert (ip);
+    return SUCCESS;
+  }
+  return IP_EXISTS;
+}
+
+uint32_t Volatile_Metadata::set_node_down (char *ip) {
+  if (up_nodes_contains (ip)) {
+    up_nodes.remove (ip);
+    return SUCCESS;
+  }
+  return IP_NOT_FOUND;
+}
+
+uint32_t Volatile_Metadata::get_active_nodes (char **nodes) {
+  int count = 0;
+  *nodes = realloc (up_nodes.size());
+
+  for (std::list<string>::iterator it = up_nodes.being(); 
+       it != up_nodes.end(); it++) {
+    nodes[count] = malloc (*it.length());
+    memcpy (nodes[count++], (*it.c_str()), *it.length());
+  }
+
+  return count;
+}
+
+bool Volatile_Metadata::ip_to_node_map_contains (char *ip) {
+  return (ip_to_node_map.find (ip) == ip_to_node_map.end());
+}
+
+bool Volatile_Metadata::up_nodes_contains (char *ip) {
+  return (std::find (up_nodes.begin(), up_nodes.end(), ip) != up_nodes.end());
 }
--- src/app/barista_core/volatile_metadata.h
+++ src/app/barista_core/volatile_metadata.h
@@ -3,28 +3,96 @@
 
 #include <stdint.h>
 
+#include <list>
+
 #include "limits.h"
 
+#define SUCCESS 0
+
+// Chunk and Stripe Size Errors
 #define SIZE_ALREADY_SET -1
 #define SIZE_INVALID -2
+
+// Map Errors
+#define IP_EXISTS -1
+#define IP_NOT_FOUND -2
 
 using namespace std;
 
 class Volatile_Metadata {
   private:
+    // Variables
     uint32_t chunk_size;
     uint32_t stripe_size;
+    std::map<string, int> ip_to_node_map;
+    std::list<string> up_nodes;
     // TODO: add file cursors and node map
+
+    // Helper Functions
+    bool ip_to_node_map_contains (char *ip);
+    bool up_nodes_contains (char *ip);
 
   public:
     Volatile_Metadata();
-    std::map<string, int> ip_to_node_map;
 
+    /*
+     * Returns the chunk size that is set for this instance of DecaFS.
+     * If chunk size has not been set yet, this function returns 0.
+     */
     uint32_t get_chunk_size ();
+    /*
+     * Sets the chunk size for this instance of DecaFS.
+     * If chunk size has already been set, SIZE_ALREADY_SET is returned.
+     * If the chunk size provided is an invalid size SIZE_INVALID is returned.
+     */
     int set_chunk_size (uint32_t size);
 
-    uint32_t get_stripe_size();
+    /*
+     * Returns the stripe size that is set for this instance of DecaFS.
+     * If stripe size has not been set yet, this function returns 0.
+     */
+    uint32_t get_stripe_size ();
+    /*
+     * Sets the stripe size for this instance of DecaFS.
+     * If stripe size has already been set, SIZE_ALREADY_SET is returned.
+     * If the stripe size provided is an invalid size SIZE_INVALID is returned.
+     */
     int set_stripe_size (uint32_t size);
 
+    /*
+     * Returns the node number for a specific IP address.
+     * If barista_core was not started with the given IP address, IP_NOT_FOUND
+     *   is returned.
+     */
+    uint32_t get_node_number (char *ip);
+    /*
+     * Add a node ip and node number pairing to the metadata for this instance
+     *   of DecaFS.
+     * If the ip address has already been assigned a node number, IP_EXISTS
+     *   is returned.
+     */
+    int add_node (char *ip, uint32_t node_number);
+    /*
+     * Set the node with the specific ip address to be "down" 
+     *   of DecaFS.
+     * If barista_core was not started with the given IP address, IP_NOT_FOUND
+     *   is returned.
+     */
+    uint32_t set_node_down (char *ip);
+    /*
+     * Set the node with the specific ip address to be "up" 
+     *   of DecaFS.
+     * If barista_core was not started with the given IP address, IP_NOT_FOUND
+     *   is returned.
+     */
+    uint32_t set_node_up (char *ip);
+    /*
+     * Give the "state" of the system.
+     * nodes is filled in with the ip addresses fo the active nodes (nodes
+     *   that were not set to "down").
+     * nodes will be reallocated to support the number of nodes that are up.
+     * The number of active nodes in the system is returned.
+     */
+    uint32_t get_active_nodes (char **nodes);
 }; 
 #endif
