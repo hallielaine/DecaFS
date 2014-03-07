617616b5a59a9d2b967d29a282d6a94c0623ecaa
PeterFaiman <peterfaiman@gmail.com>
2014-03-06 19:15:43 -0800

    modified makefiles to build espresso binary
    libnetcore is commented out of the espresso build, since it breaks the
    build

--- src/app/espresso_core/espresso_core.cc
+++ src/app/espresso_core/espresso_core.cc
@@ -1,10 +1,12 @@
 #include "espresso_core.h"
 
 int main(int argc, char** argv) {
+  if (argc < 2)
+    return 1;
 
   printf("hello from espresso %s!\n", argv[1]);
 
-  svc_main_loop(0, NULL); // svc_main_loop(int argc, char** argv);
+  // svc_main_loop(int argc, char** argv);
 
   return 0;
 }
