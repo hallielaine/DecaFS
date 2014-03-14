1fcbeacd54be0e716ad7273fdaa53b29095d07c2
PeterFaiman <peterfaiman@gmail.com>
2014-03-14 02:46:05 -0700

    DECAFS-31: more tests

--- test/locking_strategy_test/locking_strategy_unittest.cc
+++ test/locking_strategy_test/locking_strategy_unittest.cc
@@ -36,12 +36,10 @@ std::thread locked_write(int *d, int i, int u, int p, int f, int t, int b, int a
 
 TEST(LockingTest, SimpleRead) {
   int i = 7;
-  auto start = std::chrono::high_resolution_clock::now();
-  auto r1 = locked_read(&i, 1, 1, 1, -1, 1, 1);
+  auto r1 = locked_read(&i, 1, 1, 1, -1, 0, 1);
+  ASSERT_EQ(std::future_status::ready, r1.wait_for(BASE_DURATION(1.5)));
   int r1r = r1.get();
-  auto end = std::chrono::high_resolution_clock::now();
   EXPECT_EQ(i, r1r);
-  EXPECT_GT(end - start, std::chrono::milliseconds(150));
 }
 
 TEST(LockingTest, SimpleWrite) {
@@ -49,11 +47,10 @@ TEST(LockingTest, SimpleWrite) {
   auto start = std::chrono::high_resolution_clock::now();
   auto w1 = locked_write(&i, 7, 1, 1, 1, -1, 0, 2);
   auto r1 = locked_read(&i, 1, 2, 1, -1, 1, 0);
-  int r1r = r1.get();
+  ASSERT_EQ(std::future_status::ready, r1.wait_for(BASE_DURATION(2.5)));
   auto end = std::chrono::high_resolution_clock::now();
   EXPECT_GT(end - start, BASE_DURATION(1.5));
-  EXPECT_LT(end - start, BASE_DURATION(2.5));
-  EXPECT_EQ(7, r1r);
+  EXPECT_EQ(7, r1.get());
   w1.join();
 }
 
@@ -64,17 +61,17 @@ TEST(LockingTest, MultiRead) {
   auto r2 = locked_read(&i, 1, 2, 1, -1, 0, 1);
   auto r3 = locked_read(&i, 1, 3, 1, -1, 0, 1);
   auto r4 = locked_read(&i, 1, 4, 1, -1, 0, 1);
-  int r1r = r1.get();
-  int r2r = r2.get();
-  int r3r = r3.get();
-  int r4r = r4.get();
+  ASSERT_EQ(std::future_status::ready, r1.wait_until(start + BASE_DURATION(1.5)));
+  ASSERT_EQ(std::future_status::ready, r2.wait_until(start + BASE_DURATION(1.5)));
+  ASSERT_EQ(std::future_status::ready, r3.wait_until(start + BASE_DURATION(1.5)));
+  ASSERT_EQ(std::future_status::ready, r4.wait_until(start + BASE_DURATION(1.5)));
   auto end = std::chrono::high_resolution_clock::now();
   EXPECT_GT(end - start, BASE_DURATION(0.5));
   EXPECT_LT(end - start, BASE_DURATION(1.5));
-  EXPECT_EQ(7, r1r);
-  EXPECT_EQ(7, r2r);
-  EXPECT_EQ(7, r3r);
-  EXPECT_EQ(7, r4r);
+  EXPECT_EQ(7, r1.get());
+  EXPECT_EQ(7, r2.get());
+  EXPECT_EQ(7, r3.get());
+  EXPECT_EQ(7, r4.get());
 }
 
 TEST(LockingTest, MultiUserRead) {
@@ -84,18 +81,46 @@ TEST(LockingTest, MultiUserRead) {
   auto r2 = locked_read(&i, 1, 2, 1, -1, 0, 2);
   auto r3 = locked_read(&i, 2, 1, 1, -1, 1, 1);
   auto r4 = locked_read(&i, 2, 2, 1, -1, 1, 1);
-  int r1r = r1.get();
-  int r2r = r2.get();
+  ASSERT_EQ(std::future_status::ready, r1.wait_until(start + BASE_DURATION(2.5)));
+  ASSERT_EQ(std::future_status::ready, r2.wait_until(start + BASE_DURATION(2.5)));
   auto end1 = std::chrono::high_resolution_clock::now();
-  int r3r = r3.get();
-  int r4r = r4.get();
+  ASSERT_EQ(std::future_status::ready, r3.wait_until(start + BASE_DURATION(3.5)));
+  ASSERT_EQ(std::future_status::ready, r4.wait_until(start + BASE_DURATION(3.5)));
   auto end2 = std::chrono::high_resolution_clock::now();
   EXPECT_GT(end1 - start, BASE_DURATION(1.5));
   EXPECT_LT(end1 - start, BASE_DURATION(2.5));
   EXPECT_GT(end2 - start, BASE_DURATION(2.5));
   EXPECT_LT(end2 - start, BASE_DURATION(3.5));
-  EXPECT_EQ(7, r1r);
-  EXPECT_EQ(7, r2r);
-  EXPECT_EQ(7, r3r);
-  EXPECT_EQ(7, r4r);
+  EXPECT_EQ(7, r1.get());
+  EXPECT_EQ(7, r2.get());
+  EXPECT_EQ(7, r3.get());
+  EXPECT_EQ(7, r4.get());
+}
+
+TEST(LockingTest, MultipleExclusive) {
+  int i = 0;
+  auto start = std::chrono::high_resolution_clock::now();
+  auto w1 = locked_write(&i, 7, 1, 1, 1, -1, 0, 2);
+  auto w2 = locked_write(&i, 8, 1, 1, 1, -1, 1, 1);
+  std::this_thread::sleep_until(start + BASE_DURATION(.5));
+  EXPECT_EQ(7, i);
+  w1.join();
+  auto end1 = std::chrono::high_resolution_clock::now();
+  std::this_thread::sleep_until(start + BASE_DURATION(2.5));
+  EXPECT_EQ(8, i);
+  w2.join();
+  auto end2 = std::chrono::high_resolution_clock::now();
+  EXPECT_GT(end1 - start, BASE_DURATION(1.5));
+  EXPECT_LT(end1 - start, BASE_DURATION(2.5));
+  EXPECT_GT(end2 - start, BASE_DURATION(2.5));
+  EXPECT_LT(end2 - start, BASE_DURATION(3.5));
+}
+
+TEST(LockingTest, SameUserExclusiveAndShared) {
+  int i = 0;
+  auto w1 = locked_write(&i, 7, 1, 1, 1, -1, 0, 2);
+  auto r1 = locked_read(&i, 1, 2, 1, -1, 1, 0);
+  ASSERT_EQ(std::future_status::ready, r1.wait_for(BASE_DURATION(2.5)));
+  EXPECT_EQ(7, r1.get());
+  w1.join();
 }
