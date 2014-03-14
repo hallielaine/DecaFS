9ab04797de1de43100c09d7672da91eca1fee7ea
PeterFaiman <peterfaiman@gmail.com>
2014-03-13 21:40:56 -0700

    DECAFS-30: new implementation, still untested

--- src/lib/locking_strategy/locking_strategy.cc
+++ src/lib/locking_strategy/locking_strategy.cc
@@ -13,172 +13,190 @@
 
 struct file_lock {
   std::mutex m;
-  std::condition_variable cv;
+  std::condition_variable ex_cv;
+  std::condition_variable sh_cv;
   int owner;
-  int exclusive_proc;
-  std::set<int> shared_procs;
-  bool exclusive_intent;
+  int ex_intent;
+  int ex_proc;
+  int sh_intent;
+  std::set<int> sh_procs;
 
-  file_lock() : owner(-1), exclusive_proc(-1), exclusive_intent(false) {}
+  file_lock() : owner(-1), ex_intent(0), ex_proc(-1), sh_intent(0) {}
 };
 
-static std::mutex regular_m;
-static std::unordered_map<int, file_lock> regular_locks;
-static std::mutex metadata_m;
-static std::unordered_map<int, file_lock> metadata_locks;
+static std::mutex reg_m;
+static std::unordered_map<int, file_lock> reg_locks;
+static std::mutex meta_m;
+static std::unordered_map<int, file_lock> meta_locks;
 
-inline int exlock(std::mutex &m, std::unordered_map<int, file_lock> &locks,
-    int user_id, int proc_id, int file_id, int timeout) {
+file_lock& reg_fl(int file_id) {
+  std::lock_guard<std::mutex> lk(reg_m);
+  return reg_locks[file_id];
+}
+
+file_lock& meta_fl(int file_id) {
+  std::lock_guard<std::mutex> lk(meta_m);
+  return meta_locks[file_id];
+}
+
+inline int ex_lock(file_lock &fl, int user_id, int proc_id, int timeout) {
   if (timeout < 0) {
-    // lock around accessing the map
-    file_lock *fl;
-    {
-      std::lock_guard<std::mutex> lk(m);
-      fl = &locks[file_id];
-    }
-
     // acquire mutation lock for this file_lock
-    std::unique_lock<std::mutex> lk(fl->m);
+    std::unique_lock<std::mutex> lk(fl.m);
 
-    if (user_id != fl->owner) {
-      fl->cv.wait(lk, [fl]{ return fl->owner == -1; });
+    if (user_id != fl.owner) {
+      // wait for the owner to release the file or another process of this
+      // user_id to acquire it
+      fl.ex_intent++;
+      fl.ex_cv.wait(lk, [&fl, user_id]{ return fl.owner == -1 ||
+                                               fl.owner == user_id; });
+      fl.ex_intent--;
 
-      // if just gaining ownership, lock should be all clear
-      assert(!fl->exclusive_intent);
-      assert(fl->exclusive_proc == -1);
-      assert(fl->shared_procs.size() == 0);
+      if (fl.owner == -1) {
+        // an unowned file should not have active locks...
+        assert(fl.ex_proc == -1);
+        assert(fl.sh_procs.size() == 0);
 
-      fl->owner = user_id;
-      fl->exclusive_proc = proc_id;
-      return 0;
-    }
-    if (fl->exclusive_intent) {
-      fl->cv.wait(lk, [fl]{ return !fl->exclusive_intent; });
-      fl->exclusive_intent = true;
-    }
-
-    // at this point this user_id owns the file_lock and has exclusive_intent
-
-    if (fl->exclusive_proc != -1 || fl->shared_procs.size() != 0) {
-      fl->cv.wait(lk, [fl]{ return fl->exclusive_proc == -1 &&
-          fl->shared_procs.size() == 0; });
+        // acquire ownership
+        fl.owner = user_id;
+      }
     }
 
-    fl->exclusive_proc = proc_id;
-    fl->exclusive_intent = false;
+    if (fl.ex_proc != -1 || fl.sh_procs.size() != 0) {
+      // wait for all locks to be released
+      fl.ex_intent++;
+      fl.ex_cv.wait(lk, [&fl]{ return fl.ex_proc == -1 &&
+                                      fl.sh_procs.size() == 0; });
+      fl.ex_intent--;
+    }
+
+    // acquire exclusive lock
+    fl.ex_proc = proc_id;
     return 0;
   }
 
   return -1;
 }
 
-inline int shlock(std::mutex &m, std::unordered_map<int, file_lock> &locks,
-    int user_id, int proc_id, int file_id, int timeout) {
+inline int sh_lock(file_lock &fl, int user_id, int proc_id, int timeout) {
   if (timeout < 0) {
-    file_lock *fl;
-    {
-      std::lock_guard<std::mutex> lk(m);
-      fl = &locks[file_id];
-    }
-
     // acquire mutation lock for this file_lock
-    std::unique_lock<std::mutex> lk(fl->m);
+    std::unique_lock<std::mutex> lk(fl.m);
 
-    if (user_id != fl->owner) {
-      fl->cv.wait(lk, [fl]{ return fl->owner == -1; });
+    if (user_id != fl.owner) {
+      // wait for the owner to release the file or another process of this
+      // user_id to acquire it
+      fl.sh_intent++;
+      fl.sh_cv.wait(lk, [&fl, user_id]{ return fl.owner == -1 ||
+                                               fl.owner == user_id; });
+      fl.sh_intent--;
 
-      // if just gaining ownership, lock should be all clear
-      assert(!fl->exclusive_intent);
-      assert(fl->exclusive_proc == -1);
-      assert(fl->shared_procs.size() == 0);
+      if (fl.owner == -1) {
+        // an unowned file should not have active locks...
+        assert(fl.ex_proc == -1);
+        assert(fl.sh_procs.size() == 0);
 
-      fl->owner = user_id;
-      fl->shared_procs.insert(proc_id);
-      return 0;
+        // acquire ownership
+        fl.owner = user_id;
+      }
     }
-    if (fl->exclusive_proc || fl->exclusive_intent) {
-      fl->cv.wait(lk, [fl]{ return !fl->exclusive_proc &&
-          !fl->exclusive_intent; });
+
+    if (fl.ex_proc != -1) {
+      // wait for the exclusive lock to be released
+      fl.sh_intent++;
+      fl.sh_cv.wait(lk, [&fl]{ return fl.ex_proc == -1; });
+      fl.sh_intent--;
     }
 
-    auto r = fl->shared_procs.insert(proc_id);
+    // acquire shared lock
+    auto r = fl.sh_procs.insert(proc_id);
     return r.second ? 0 : -1;
   }
+
 
   return -1;
 }
 
-static int rlock(std::mutex &m, std::unordered_map<int, file_lock> &locks,
-    int user_id, int proc_id, int file_id) {
-  file_lock *fl;
-  {
-    std::lock_guard<std::mutex> lk(m);
-    fl = &locks[file_id];
-  }
-
-  std::unique_lock<std::mutex> lk(fl->m);
-  if (user_id != fl->owner)
+static int exsh_unlock(file_lock &fl, int user_id, int proc_id) {
+  std::unique_lock<std::mutex> lk(fl.m);
+  if (user_id != fl.owner)
     return -1;
-  if (proc_id == fl->exclusive_proc) {
-    fl->exclusive_proc = -1;
+  if (proc_id == fl.ex_proc) {
+    fl.ex_proc = -1;
   } else {
-    const auto flit = fl->shared_procs.find(proc_id);
-    if (flit == fl->shared_procs.end())
+    const auto flit = fl.sh_procs.find(proc_id);
+    if (flit == fl.sh_procs.end())
       return -1;
-    fl->shared_procs.erase(flit);
+    fl.sh_procs.erase(flit);
   }
 
-  lk.unlock();
-  fl->cv.notify_all();
+  if (fl.ex_intent > 0) {
+    fl.ex_cv.notify_one();
+  } else if (fl.sh_intent > 0) {
+    fl.sh_cv.notify_all();
+  } else {
+    // TODO: delete the record instead
+    fl.owner = -1;
+  }
   return 0;
 }
 
 extern "C"
 int get_exclusive_lock(int user_id, int proc_id, int file_id, int timeout) {
-  return exlock(regular_m, regular_locks, user_id, proc_id, file_id, timeout);
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
+  return ex_lock(reg_fl(file_id), user_id, proc_id, timeout);
 }
 
 extern "C"
 int get_shared_lock(int user_id, int proc_id, int file_id, int timeout) {
-  return shlock(regular_m, regular_locks, user_id, proc_id, file_id, timeout);
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
+  return sh_lock(reg_fl(file_id), user_id, proc_id, timeout);
 }
 
 extern "C"
 int release_lock(int user_id, int proc_id, int file_id) {
-  return rlock(regular_m, regular_locks, user_id, proc_id, file_id);
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
+  return exsh_unlock(reg_fl(file_id), user_id, proc_id);
 }
 
 extern "C"
 int has_exclusive_lock(int user_id, int proc_id, int file_id) {
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
   return -1;
 }
 
 extern "C"
 int has_shared_lock(int user_id, int proc_id, int file_id) {
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
   return -1;
 }
 
 extern "C"
 int get_exclusive_metadata_lock(int user_id, int proc_id, int file_id, int timeout) {
-  return exlock(metadata_m, metadata_locks, user_id, proc_id, file_id, timeout);
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
+  return ex_lock(meta_fl(file_id), user_id, proc_id, timeout);
 }
 
 extern "C"
 int get_shared_metadata_lock(int user_id, int proc_id, int file_id, int timeout) {
-  return shlock(metadata_m, metadata_locks, user_id, proc_id, file_id, timeout);
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
+  return sh_lock(meta_fl(file_id), user_id, proc_id, timeout);
 }
 
 extern "C"
 int release_metadata_lock(int user_id, int proc_id, int file_id) {
-  return rlock(metadata_m, metadata_locks, user_id, proc_id, file_id);
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
+  return exsh_unlock(meta_fl(file_id), user_id, proc_id);
 }
 
 extern "C"
 int has_exclusive_metadata_lock(int user_id, int proc_id, int file_id) {
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
   return -1;
 }
 
 extern "C"
 int has_shared_metadata_lock(int user_id, int proc_id, int file_id) {
+  if (user_id < 0 || proc_id < 0 || file_id < 0) return -1;
   return -1;
 }
