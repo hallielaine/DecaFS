f7526d65ce138ec6361c4d13f15c20f773ea25ba
Jeffrey Forrester <jeffrey_forrester@yahoo.com>
2014-04-01 14:05:48 -0700

    created access module, linked it to barista module so process_... can be used there. fixed makefile issue where libraries were incorrectly linked in barista

---
+++ src/lib/access/access.h
@@ -0,0 +1,21 @@
+#include "../network_core/bar_clnt.h"
+
+/*
+ *	Read data from a chunk at a specific offset.
+ *	If you are implementing this function: 
+ *		If data is being read from an Espresso node, Network
+ *		Layer network_read_chunk() must be called.
+ */
+ssize_t process_read_chunk (int fd, int file_id, int node_id, 
+     int stripe_id, int chunk_num, int offset, 
+     void *buf, int count);
+
+/*
+ *	Write data to a chunk at a specific offset.
+ *	If you are implementing this function: 
+ *		If data is being written to an Espresso node, Network
+ *		Layer network_write_chunk() must be called.
+ */
+ssize_t process_write_chunk (int fd, int file_id, int node_id, 
+ int stripe_id, int chunk_num, int offset, 
+ void *buf, int count);
