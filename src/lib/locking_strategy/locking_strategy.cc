8dd54cbcb62cba9fef9c447b5f069c6a2d886f3f
Peter Faiman <peterfaiman@gmail.com>
2014-04-06 03:36:22 -0700

    finish locking strategy impl

--- src/lib/locking_strategy/locking_strategy.cc
+++ src/lib/locking_strategy/locking_strategy.cc
@@ -1,34 +1,40 @@
 #include "locking_strategy.h"
 
 #include <cassert>
-#include <unordered_map>
-#include <set>
-#include <utility>
-#include <mutex>
+#include <cstdint>
+
 #include <chrono>
-
-
-#define UNUSED __attribute__((unused));
+#include <mutex>
+#include <set>
+#include <unordered_map>
+#include <utility>
 
 
 struct file_lock {
-  int owner;
-  int ex_proc;
+  uint32_t owner;
+  uint32_t ex_proc;
   std::set<int> sh_procs;
-  file_lock() : owner(-1), ex_proc(-1) {}
+  file_lock() : owner(0), ex_proc(0), sh_procs() {}
+};
+
+struct meta_lock {
+  std::mutex mtx;
+  uint32_t owner;
+  uint32_t proc;
+  meta_lock() : mtx(), owner(0), proc(0) {}
 };
 
 static std::mutex file_m;
-static std::unordered_map<int, file_lock> file_locks;
+static std::unordered_map<uint32_t, file_lock> file_locks;
 static std::mutex meta_m;
-static std::unordered_map<int, std::mutex> meta_locks;
+static std::unordered_map<uint32_t, meta_lock> meta_locks;
 
 extern "C"
-int get_exclusive_lock(int user_id, int proc_id, int file_id) {
+int get_exclusive_lock(uint32_t user_id, uint32_t proc_id, uint32_t file_id) {
   std::lock_guard<std::mutex> lk(file_m);
   file_lock &lock = file_locks[file_id];
 
-  if (lock.owner != -1)
+  if (lock.owner)
     return -1;
 
   lock.owner = user_id;
@@ -37,12 +43,12 @@ int get_exclusive_lock(int user_id, int proc_id, int file_id) {
 }
 
 extern "C"
-int get_shared_lock(int user_id, int proc_id, int file_id) {
+int get_shared_lock(uint32_t user_id, uint32_t proc_id, uint32_t file_id) {
   std::lock_guard<std::mutex> lk(file_m);
   file_lock &lock = file_locks[file_id];
 
   if (lock.owner != user_id) {
-    if (lock.owner != -1)
+    if (lock.owner)
       return -1;
 
     lock.owner = user_id;
@@ -53,7 +59,7 @@ int get_shared_lock(int user_id, int proc_id, int file_id) {
 }
 
 extern "C"
-int release_lock(int user_id, int proc_id, int file_id) {
+int release_lock(uint32_t user_id, uint32_t proc_id, uint32_t file_id) {
   std::lock_guard<std::mutex> lk(file_m);
   file_lock &lock = file_locks[file_id];
 
@@ -75,7 +81,7 @@ int release_lock(int user_id, int proc_id, int file_id) {
 }
 
 extern "C"
-int has_exclusive_lock(int user_id, int proc_id, int file_id) {
+int has_exclusive_lock(uint32_t user_id, uint32_t proc_id, uint32_t file_id) {
   std::lock_guard<std::mutex> lk(file_m);
   auto lock_it = file_locks.find(file_id);
 
@@ -92,7 +98,7 @@ int has_exclusive_lock(int user_id, int proc_id, int file_id) {
 }
 
 extern "C"
-int has_shared_lock(int user_id, int proc_id, int file_id) {
+int has_shared_lock(uint32_t user_id, uint32_t proc_id, uint32_t file_id) {
   std::lock_guard<std::mutex> lk(file_m);
   auto lock_it = file_locks.find(file_id);
 
@@ -110,3 +116,52 @@ int has_shared_lock(int user_id, int proc_id, int file_id) {
   return 1;
 }
 
+extern "C"
+int get_metadata_lock(uint32_t user_id, uint32_t proc_id, uint32_t file_id) {
+  meta_lock *m_lk = NULL;
+  {
+    std::lock_guard<std::mutex> lk(meta_m);
+    m_lk = &meta_locks[file_id];
+  }
+
+  m_lk->mtx.lock();
+
+  if (m_lk->owner || m_lk->proc) {
+    m_lk->mtx.unlock();
+    return -1;
+  }
+
+  m_lk->owner = user_id;
+  m_lk->proc = proc_id;
+  return 0;
+}
+
+extern "C"
+int release_metadata_lock(uint32_t user_id, uint32_t proc_id, uint32_t file_id) {
+  std::unordered_map<uint32_t, meta_lock>::iterator meta_it;
+  {
+    std::lock_guard<std::mutex> lk(meta_m);
+    meta_it = meta_locks.find(file_id);
+  }
+
+  if (meta_it == meta_locks.end() ||
+      meta_it->second.owner != user_id ||
+      meta_it->second.proc != proc_id) {
+    return -1;
+  }
+
+  meta_it->second.owner = 0;
+  meta_it->second.proc = 0;
+  meta_it->second.mtx.unlock();
+  return 0;
+}
+
+extern "C"
+int has_metadata_lock(uint32_t user_id, uint32_t proc_id, uint32_t file_id) {
+  std::lock_guard<std::mutex> lk(meta_m);
+  auto meta_it = meta_locks.find(file_id);
+
+  return meta_it != meta_locks.end() &&
+         meta_it->second.owner == user_id &&
+         meta_it->second.proc == proc_id;
+}
