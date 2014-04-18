009938f365bb3e3577e4eb5d429689d3e27e4f4b
PeterFaiman <peterfaiman@gmail.com>
2014-04-17 22:29:34 -0700

    stdio must be included to use stdio

--- src/lib/network_core/bar_clnt.h
+++ src/lib/network_core/bar_clnt.h
@@ -1,6 +1,7 @@
 #ifndef _BAR_CLNT_H
 #define _BAR_CLNT_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "network.h"
@@ -12,9 +13,9 @@ extern "C" {
 #endif
 
 extern int network_add_client(char*);
-extern ssize_t network_read_chunk(int fd, int file_id, int node_id, int stripe_id, int chunk_num, 
+extern ssize_t network_read_chunk(int fd, int file_id, int node_id, int stripe_id, int chunk_num,
                            int offset, void* buf, int count);
-extern ssize_t network_write_chunk(int fd, int file_id, int node_id, int stripe_id, int chunk_num, 
+extern ssize_t network_write_chunk(int fd, int file_id, int node_id, int stripe_id, int chunk_num,
                            int offset, const void* buf, int count);
 extern int network_delete_chunk(int fd, int file_id, int node_id, int stripe_id, int chunk_num);
 
