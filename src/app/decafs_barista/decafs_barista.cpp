2d2d9a04b9cef5175fff0aabbdee8b22f1a91c19
Halli Meth <hmeth@calpoly.edu>
2014-04-21 20:01:04 -0700

    barista main.

--- src/app/decafs_barista/decafs_barista.cpp
+++ src/app/decafs_barista/decafs_barista.cpp
@@ -6,15 +6,20 @@
 #define METADATA 3
 #define ESPRESSO 4
 
+BaristaServer *barista_server;
+
 int main (int argc, char *argv[]) {
   process_arguments (argc, argv);
-
-  // TODO: Change to proper logging
   printf ("Barista is initialized.\n");
   printf ("\tstripe_size: %d\n\tchunk_size: %d\n\n", get_stripe_size(),
            get_chunk_size());
-   
-  struct ip_address ip;
+
+                                    // TODO: Make port num an arg
+  barista_server = new BaristaServer(1034);
+  barista_server->run();
+
+  /* TEST CODE */
+  /*struct ip_address ip;
   struct client default_client = {ip, 1, 1};
   int count, fd = open_file ("new_file.txt", O_RDWR, default_client);
   char buf[] = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100";
@@ -44,7 +49,7 @@ int main (int argc, char *argv[]) {
   printf ("\n\n ------------------------------SECOND READ--------------------------------\n");
   count = read_file (fd, read_buf, 2*strlen (buf), default_client);
   printf ("\n(BARISTA) Read %d bytes.\n", count);
-  printf ("(BARISTA) Buf is:\n%s\n", read_buf);
+  printf ("(BARISTA) Buf is:\n%s\n", read_buf);*/
   return 0;
 }
 
