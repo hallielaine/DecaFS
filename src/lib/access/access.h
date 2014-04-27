46cc51239ae27aa17b9098f5b099b80c74f607e6
Halli Meth <hmeth@calpoly.edu>
2014-04-27 13:57:06 -0700

    Updated access to send network layer request id.

--- src/lib/access/access.h
+++ src/lib/access/access.h
@@ -10,8 +10,9 @@
  *		If data is being read from an Espresso node, Network
  *		Layer network_read_chunk() must be called.
  */
-ssize_t process_read_chunk (int fd, int file_id, int node_id, int stripe_id,
-     int chunk_num, int offset, void* buf, int count);
+ssize_t process_read_chunk (uint32_t request_id, int fd, int file_id,
+                            int node_id, int stripe_id, int chunk_num,
+                            int offset, void* buf, int count);
 
 /*
  *	Write data to a chunk at a specific offset.
@@ -19,9 +20,12 @@ ssize_t process_read_chunk (int fd, int file_id, int node_id, int stripe_id,
  *		If data is being written to an Espresso node, Network
  *		Layer network_write_chunk() must be called.
  */
-ssize_t process_write_chunk (int fd, int file_id, int node_id, int stripe_id,
- int chunk_num, int offset, void *buf, int count);
+ssize_t process_write_chunk (uint32_t request_id, int fd, int file_id,
+                             int node_id, int stripe_id, int chunk_num,
+                             int offset, void *buf, int count);
 
-
-ssize_t process_delete_chunk (int file_id, int node_id, int stripe_id,
- int chunk_num);
+/*
+ * Delete a specific chunk from DecaFS.
+ */
+ssize_t process_delete_chunk (uint32_t request_id, int file_id, int node_id,
+                              int stripe_id, int chunk_num);
