ce807f6361c0a51b9d3e9ced0a0de269e0b38bb4
Jeffrey Forrester <jeffrey_forrester@yahoo.com>
2014-04-27 14:52:07 -0700

    added close packet and close packet response to network_core

---
+++ src/lib/network_core/close_packet.cpp
@@ -0,0 +1,26 @@
+#include "close_packet.h"
+
+ClosePacket::ClosePacket(void* buf, ssize_t size) : Packet(buf, size) {
+
+  uint32_t* base = (uint32_t*)(((uint8_t*)buf) + Packet::dataSize());
+  fd = base[0];
+}
+
+ClosePacket::ClosePacket(int fd) : Packet(0, ESPRESSO_INIT, data_size),
+  fd(fd) {
+
+  uint32_t* base = (uint32_t*)(((uint8_t*)packet) + Packet::dataSize());
+  base[0] = fd;
+}
+
+std::ostream& ClosePacket::print(std::ostream& stream) const {
+
+  stream << "ClosePacketializationPacket" << std::endl;
+  stream << "\tfd: " << fd << std::endl;
+  return Packet::print(stream); 
+}
+
+std::ostream& operator<<(std::ostream& stream, const ClosePacket &p) {
+  return p.print(stream); 
+}
+
