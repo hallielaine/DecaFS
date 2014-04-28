e46bf1fa619459c719806e2d99d19619932f27a5
Jeffrey Forrester <jeffrey_forrester@yahoo.com>
2014-04-28 13:28:59 -0700

    adding a sample decafs client for testing purposes

---
+++ src/app/decafs_test_client/main.cpp
@@ -0,0 +1,46 @@
+#include "network_core/barista_server.h"
+#include "network_core/espresso_client.h"
+#include "../decafs_barista/decafs_barista.h"
+#include "network_core/decafs_client.h"
+
+#include <stdio.h>
+#include <thread>
+#include "network_core/barista_network_helper.h"
+
+int main(int argc, char** argv) {
+
+  int port = 1234;
+
+  DecafsClient client("192.168.1.100", port, 2);
+  client.openConnection();
+
+  sleep(1);
+
+  // OPEN
+  std::cout << "------------ DECAFS CLIENT OPEN TEST ----------" << std::endl;
+  int fd = client.open("testfile", O_RDWR);
+  std::cout << "open returned: " << fd << std::endl;
+  sleep(1);
+
+  // WRITE
+  std::cout << "------------ DECAFS CLIENT WRITE TEST ----------" << std::endl;
+  char testwrite[] = "testing network write.";
+  int bytes_written = client.write(fd, testwrite, strlen(testwrite));
+  std::cout << "write returned: " << bytes_written << std::endl;
+  sleep(1);
+
+  // READ
+  std::cout << "------------ DECAFS CLIENT READ TEST ----------" << std::endl;
+  char testread[100];
+  int bytes_read = client.read(fd, testread, 100);
+  std::cout << "read returned: " << bytes_read << std::endl;
+  sleep(1);
+
+  // CLOSE
+  std::cout << "------------ DECAFS CLIENT CLOSE TEST ----------" << std::endl;
+  int close = client.close(fd);
+  std::cout << "close returned: " << close << std::endl;
+  sleep(1);
+
+  return 0;
+}
