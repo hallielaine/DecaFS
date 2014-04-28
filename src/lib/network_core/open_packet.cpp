ce0a35dcfb301c8fac44d9b8a420d67b30b73ab8
Jeffrey Forrester <jeffrey_forrester@yahoo.com>
2014-04-27 21:35:45 -0700

    fixed seg fault issue in open packet

--- src/lib/network_core/open_packet.cpp
+++ src/lib/network_core/open_packet.cpp
@@ -1,25 +1,26 @@
 #include "open_packet.h"
 
 OpenPacket::OpenPacket(int flags, char* filepath) 
- : Packet(0, OPEN, strlen(filepath) + sizeof(int)),
+ : Packet(0, OPEN, strlen(filepath) + 1 + sizeof(int)),
  open_flags(flags), filepath(filepath) {
 
   uint32_t* base = (uint32_t*)(((uint8_t*)packet) + Packet::dataSize());
   base[0] = open_flags; 
 
   char* pathbase = ((char*)base) + sizeof(open_flags);
-  strncpy(pathbase, filepath, strlen(filepath));
+  strncpy(pathbase, filepath, strlen(filepath)+1);
 }
 
 OpenPacket::OpenPacket(void* buf, ssize_t length) : Packet(buf, length) {
 
   uint32_t* base = (uint32_t*)(((uint8_t*)packet) + Packet::dataSize());
   open_flags = base[0];
+  filepath = (char*)base[1];
 
-  int pathlen = packet_size - Packet::dataSize() - sizeof(open_flags) + 1;
-  filepath = (char*)malloc(pathlen);
-  strncpy(filepath, (char*)base[1], pathlen-1);
-  filepath[pathlen-1] = '\0';
+  //int pathlen = packet_size - Packet::dataSize() - sizeof(open_flags) + 1;
+  //filepath = (char*)malloc(pathlen);
+  //strncpy(filepath, (char*)base[1], pathlen-1);
+  //filepath[pathlen-1] = '\0';
 }
 
 std::ostream& OpenPacket::print(std::ostream &stream) const {
