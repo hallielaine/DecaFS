dd2d970c049defe324a6d25f92ea74cd6e2671b7
Halli <hallielaine@gmail.com>
2014-05-11 04:50:07 +0000

    Change meta file names/removed double storage file.

--- src/lib/espresso_storage/espresso_state.cc
+++ src/lib/espresso_storage/espresso_state.cc
@@ -3,9 +3,9 @@
 espresso_global_data_t espresso_global_data;
 
 void espresso_global_data_init(std::string dir, int max_size) {
-  std::string data_name = dir + "espresso_data";
-  std::string metadata_name = dir + "espresso_metadata";
-  std::string free_extents_name = dir + "espresso_free_extents";
+  std::string data_name = dir + "espresso_data.dat";
+  std::string metadata_name = dir + "espresso_metadata.dat";
+  std::string free_extents_name = dir + "espresso_free_extents.dat";
 
   espresso_global_data.fd = open(data_name.c_str(), O_RDWR | O_CREAT, 0600);
   espresso_global_data.metadata.open(metadata_name.c_str());
